054da6a7f284c419f1df47810a5b2ff05367b55c
Rizki Awenk <adiwirizki207@gmail.com>
2022-03-16 16:08:24 +0700

    fix bugs error registrasi dokter

--- pages/register/clinic.jsx
+++ pages/register/clinic.jsx
@@ -117,7 +117,8 @@ function SignUpClinic() {
         }, 3000);
         route.push("/login");
       })
-      .catch(() => {
+      .catch((e) => {
+        console.log(e.response);
         swal(
           "Sorry..!",
           "register gagal, email sudah digunakan atau user sudah terdaftar",
@@ -254,7 +255,7 @@ function SignUpClinic() {
                       <div>
                         <span>Nama Lengkap</span>
                         <div className={form.input}>
-                          <input type="text" className="{form.inputStyle} " id="job" placeholder="Nama Lengkap" onChange={(e) => nameSet("dr. "+e.target.value)}/>
+                          <input type="text" className="{form.inputStyle} " id="job" placeholder="Nama Lengkap" onChange={(e) => nameSet(e.target.value)}/>
                         </div>
                       </div>
                       <div>
