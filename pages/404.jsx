139ded8461d4eefc26621859d8f62da5d0d6cc6c
akmalays <akmalays@gmail.com>
2022-03-17 00:50:54 +0700

    fix error on bind api patient, janji kunjungan dan riwayat kunjungan

---
+++ pages/404.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div>
+      <div className="bg-[#E4F5E9] h-full text-[#324B50]"></div>
+    </div>
+  );
+}
+
+export default NotFound;
